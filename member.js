502af21c52e8dd1ba95006c5e58a78c02545bfff
kallol-brc <kallol.brc@gmail.com>
2023-11-29 23:23:04 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember()
+{
+    var member = {
+        firstName: "John",
+        lastName: "Doe",
+        age: 50,
+        eyeColor: "blue"
+    };
+    alert(member.firstName + " is " + member.age + " years old.");
+}
